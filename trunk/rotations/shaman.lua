2ac46e286a8cf69700ab8591f034bcd4d1537b0e
y2kspy@gmail.com <y2kspy@gmail.com@e8371575-6b02-4a07-94a3-ac3cfb50c9e3>
2011-05-19 12:46:52 +0000

    Initial Commit

---
+++ trunk/rotations/shaman.lua
@@ -0,0 +1,237 @@
+--Shaman Rotations
+
+----------------------------------
+-------- Enhance -----------------
+----------------------------------
+
+local function CJ_CheckEnhanceBuffs()
+	local arg1,_,_,arg2 = GetWeaponEnchantInfo();
+	
+	if not arg1 then
+		CastSpell("Windfury Weapon");
+		return true;
+	end
+	
+	if not arg2 then
+		CastSpell("Flametongue Weapon");
+		return true;
+	end
+	
+	if not CJ_HasBuff("player","Lightning Shield") then
+		CastSpell("Lightning Shield");
+		return true;
+	end
+	
+	return false;
+end
+
+function CJEnhShamRot()
+	if (IsSpellInRange("Wind Shear") or IsSpellInRange("Wind Shear","focus")) and cj_interruptmode and CJCooldown("Wind Shear") == 0 and CJ_Interrupt() then
+		if UnitExists("focus") and UnitCanAttack("player","focus") then
+			TargetUnit("focus");
+			CastSpell("Wind Shear");
+			TargetLastTarget();
+		else
+			CastSpell("Wind Shear");
+		end
+	end
+
+	if not CJ_GCD() then return end; -- Check for GCD
+	if CJ_CheckEnhanceBuffs() then return end; -- Check our buffs
+	if AmIFacing == "false" then return end;
+	
+	if IsSpellInRange("Lava Lash") == 0 and IsSpellInRange("Earth Shock") == 1 then
+		if CJCooldown("Unleash Elements") == 0 then
+			CastSpell("Unleash Elements");
+			return;
+		end
+		
+		if CJCooldown("Flame Shock") == 0 then
+			if cj_aoemode and not CJ_HasDebuff("target","Flame Shock") then
+				CastSpell("Flame Shock");
+				return;
+			elseif not cj_aoemode and CJ_HasBuff("player","Unleash Flame") then
+				CastSpell("Flame Shock");
+				return;
+			end
+		end
+		
+		if cj_aoemode and CJ_HasDebuff("target","Flame Shock") and CJCooldown("Fire Nova") == 0 then
+			CastSpell("Fire Nova");
+			return;
+		end
+		
+		if CJ_BuffInfo("player","Maelstrom Weapon") == 5 then
+			if cj_aoemode and CJCooldown("Chain Lightning") == 0 then
+				CastSpell("Chain Lightning");
+				return;
+			elseif not cj_aoemode then
+				CastSpell("Lightning Bolt");
+				return;
+			end
+		end
+		
+		if CJCooldown("Earth Shock") == 0 then
+			CastSpell("Earth Shock");
+			return;
+		end
+		return;
+	elseif IsSpellInRange("Lava Lash") == 1 then
+		if CJ_UpdateTotems() then return end;
+		
+		if cj_aoemode and CJCooldown("Fire Nova") == 0 and CJ_HasDebuff("target","Flame Shock") then
+			CastSpell("Fire Nova");
+			return;
+		end
+		
+		if CJCooldown("Lava Lash") == 0 then
+			if cj_aoemode then
+				CastSpell("Lava Lash");
+				return;
+			else
+				if CJ_DebuffInfo("target","Searing Flames") == 5 or select(2,GetTotemInfo(1)) == "Fire Elemental Totem"  then
+					CastSpell("Lava Lash");
+					return;
+				end
+			end
+		end
+		
+		if CJCooldown("Flame Shock") == 0 then
+			if cj_aoemode and not CJ_HasDebuff("target","Flame Shock") then
+				CastSpell("Flame Shock");
+				return;
+			elseif not cj_aoemode and CJ_HasBuff("player","Unleash Flame") then
+				CastSpell("Flame Shock");
+				return;
+			end
+		end
+		
+		if CJ_BuffInfo("player","Maelstrom Weapon") == 5 then
+			if cj_aoemode and CJCooldown("Chain Lightning") == 0 then
+				CastSpell("Chain Lightning");
+				return;
+			elseif not cj_aoemode then
+				CastSpell("Lightning Bolt");
+				return;
+			end
+		end
+		
+		if CJCooldown("Unleash Elements") == 0 then
+			CastSpell("Unleash Elements");
+			return;
+		end
+		
+		if CJCooldown("Stormstrike") == 0 then
+			CastSpell("Stormstrike");
+			return;
+		end
+		
+		if CJCooldown("Earth Shock") == 0 and not CJ_HasBuff("player","Unleash Flame") then
+			CastSpell("Earth Shock");
+			return;
+		end
+	end			
+end
+
+------------------------------------------
+-------------- Elemental -----------------
+------------------------------------------
+
+local function CJ_CheckEleBuffs()
+	local arg1 = GetWeaponEnchantInfo();
+	
+	if not arg1 then
+		CastSpell("Flametongue Weapon");
+		return true;
+	end
+	
+	if not CJ_HasBuff("player","Lightning Shield") then
+		CastSpell("Lightning Shield");
+		return true;
+	end
+	
+	return false;
+end
+
+local function CJEleShamRot()
+	if (IsSpellInRange("Wind Shear") or IsSpellInRange("Wind Shear","focus")) and cj_interruptmode and CJCooldown("Wind Shear") == 0 and CJ_Interrupt() then
+		if UnitExists("focus") and UnitCanAttack("player","focus") then
+			TargetUnit("focus");
+			CastSpell("Wind Shear");
+			TargetLastTarget();
+		else
+			CastSpell("Wind Shear");
+		end
+	end
+
+	if not CJ_GCD() then return end; -- Check for GCD
+	if CJ_CheckEleBuffs() then return end; -- Check our buffs
+	if AmIFacing == "false" then return end;
+	
+	if CJManaPercent("player") < 50 and CJCooldown("Thunderstorm") == 0 then
+		CastSpell("Thunderstorm");
+		return;
+	end
+	
+	if GetUnitSpeed("player") > 0 and not CJ_HasBuff("player","Spiritwalker's Grace") then
+		if CJ_CheckMyCast() then
+			if CJ_UpdateTotems() then return end;
+			if CJCooldown("Thunderstorm") == 0 then CastSpell("Thunderstorm"); return; end;
+			if CJCooldown("Unleash Elements") == 0 then CastSpell("Unleash Elements"); return; end;
+			if CJCooldown("Flame Shock") == 0 then
+			
+			if cj_aoemode and CJCooldown("Fire Nova") == 0 and CJ_HasDebuff("target","Flame Shock") then
+				CastSpell("Fire Nova");
+				return;
+			end
+			
+			if cj_aoemode and not CJ_HasDebuff("target","Flame Shock") then
+					CastSpell("Flame Shock");
+					return;
+				elseif not cj_aoemode and CJ_HasBuff("player","Unleash Flame") then
+					CastSpell("Flame Shock");
+					return;
+				end
+			end
+			
+			if CJCooldown("Earth Shock") == 0 and CJ_BuffInfo("player","Lightning Shield") >= 7 then
+				CastSpell("Earth Shock");
+				return;
+			end
+		end
+	else
+		if CJ_CheckMyCast() then
+			if CJ_UpdateTotems() then return end;
+			
+			if cj_aoemode and CJCooldown("Fire Nova") == 0 and CJ_HasDebuff("target","Flame Shock") then
+				CastSpell("Fire Nova");
+				return;
+			end
+			
+			if CJCooldown("Earth Shock") == 0 and CJ_BuffInfo("player","Lightning Shield") >= 7 then
+				CastSpell("Earth Shock");
+				return;
+			end
+			
+			if cj_aoemode and CJCooldown("Chain Lightning") == 0 then
+				CastSpell("Chain Lightning");
+				return;
+			end
+			
+			if cj_aoemode and not CJ_HasDebuff("target","Flame Shock") then
+					CastSpell("Flame Shock");
+					return;
+				elseif not cj_aoemode and CJ_HasBuff("player","Unleash Flame") then
+					CastSpell("Flame Shock");
+					return;
+				end
+			end
+			
+			if CJCooldown("Lava Burst") == 0 then
+				CastSpell("Lava Burst");
+				return;
+			end
+			
+			CastSpell("Lightning Bolt");
+		end
+end
