951ce87bb1b4daa719d8e29551823b046aef17a3
y2kspy@gmail.com <y2kspy@gmail.com@e8371575-6b02-4a07-94a3-ac3cfb50c9e3>
2011-05-19 12:44:49 +0000

    Initial Commit

---
+++ trunk/rotationTable.lua
@@ -0,0 +1,34 @@
+--[[local rotationTable = {
+[12] = CJFrostDKRot(),
+[13] = CJUnholyDKRot(),
+[21] = CJBalanceDruidRot(),
+[22] = CJFeralDruidRot(),
+[31] = CJBMHunterRot(),
+[32] = CJMMHunterRot(),
+[33] = CJSVHunterRot(),
+[41] = CJArcMageRot(),
+[42] = CJFireMageRot(),
+[43] = CJFrostMageRot(),
+[53] = CJRetPallyRot(),
+[63] = CJSPriestRot(),
+[71] = CJAssRogueRot(),
+[72] = CJCombRogueRot(),
+[73] = CJSubRogueRot(),
+[81] = CJEleShamRot(),
+[82] = CJEnhShamRot(),
+[91] = CJAffLockRot(),
+[92] = CJDemoLockRot(),
+[93] = CJDestLockRot(),
+[101] = CJArmWarRot(),
+[102] = CJFuryWarRot()
+};--]]
+
+cj_rotationTable = {
+[53] = "CJRetPallyRot",
+[81] = "CJEleShamRot",
+[82] = "CJEnhShamRot"
+};
+
+cj_interruptBlacklist = {
+	["High Priestess Kilnara"] = {[1] = "Shadow Bolt"}
+};
