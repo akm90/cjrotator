7c69066c24bf97f8f1ec588a485b735dfd52e913
y2kspy@gmail.com <y2kspy@gmail.com@e8371575-6b02-4a07-94a3-ac3cfb50c9e3>
2011-05-24 11:10:49 +0000

    Added Demo Lock

--- trunk/rotationTable.lua
+++ trunk/rotationTable.lua
@@ -27,6 +27,7 @@ cj_rotationTable = {
 [81] = "CJEleShamRot",
 [82] = "CJEnhShamRot",
 [91] = "CJAffLockRot",
+[92] = "CJDemoLockRot",
 [93] = "CJDestLockRot",
 [101] = "CJArmsWarRot",
 [102]= "CJFuryWarRot"
