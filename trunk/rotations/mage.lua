21baeaa731e829b749a52ee8121cf4589faafd2f
y2kspy@gmail.com <y2kspy@gmail.com@e8371575-6b02-4a07-94a3-ac3cfb50c9e3>
2011-05-24 07:31:48 +0000

    Spellstreal GCD

--- trunk/rotations/mage.lua
+++ trunk/rotations/mage.lua
@@ -273,6 +273,7 @@ function CJFrostMageRot()
 	if cj_purgemode == true then
 		if CJ_OffensiveDispel() then
 			CastSpell("Spellsteal");
+			return;
 		end
 	end
 	if IsSpellInRange("Frostbolt") == 0 then return end;
